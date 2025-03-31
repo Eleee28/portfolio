6db93d319ad8bb422c998deede4979cfedfb80ff
Eleee28 <elena.juarrosgonzalez@gmail.com>
2025-04-01 00:06:24 +0100

    added tech carousel component

---
+++ src/data/techStack.ts
@@ -0,0 +1,111 @@
+export interface TechItem {
+	name: string;
+	src: string;
+	alt: string;
+	url: string;
+}
+
+export const techStack: TechItem[] = [
+	{
+		name: "HTML",
+		src: "/images/stack/html5-color.svg",
+		alt: "HTML",
+		url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+	},
+	{
+		name: "CSS",
+		src: "/images/stack/css-color.svg",
+		alt: "CSS",
+		url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+	},
+	{
+		name: "JavaScript",
+		src: "/images/stack/javascript-color.svg",
+		alt: "JavaScript",
+		url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+	},
+	{
+		name: "React",
+		src: "/images/stack/react-color.svg",
+		alt: "React",
+		url: "https://react.dev/",
+	},
+	{
+		name: "Astro",
+		src: "/images/stack/astro-color.svg",
+		alt: "Astro",
+		url: "https://astro.build/",
+	},
+	{
+		name: "Node.js",
+		src: "/images/stack/nodedotjs-color.svg",
+		alt: "Node.js",
+		url: "https://nodejs.org/",
+	},
+	{
+		name: "Express",
+		src: "/images/stack/express-color.svg",
+		alt: "Express",
+		url: "https://expressjs.com/",
+	},
+	{
+		name: "MongoDB",
+		src: "/images/stack/mongodb-color.svg",
+		alt: "MongoDB",
+		url: "https://www.mongodb.com/",
+	},
+	{
+		name: "PostgreSQL",
+		src: "/images/stack/postgresql-color.svg",
+		alt: "PostgreSQL",
+		url: "https://www.postgresql.org/",
+	},
+	{
+		name: "Git",
+		src: "/images/stack/git-color.svg",
+		alt: "Git",
+		url: "https://git-scm.com/",
+	},
+	{
+		name: "GitHub",
+		src: "/images/stack/github-color.svg",
+		alt: "GitHub",
+		url: "https://github.com/",
+	},
+	{
+		name: "Bash",
+		src: "/images/stack/gnubash-color.svg",
+		alt: "Bash",
+		url: "https://www.gnu.org/software/bash/",
+	},
+	{
+		name: "Linux",
+		src: "/images/stack/linux-color.svg",
+		alt: "Linux",
+		url: "https://kernel.org/",
+	},
+	{
+		name: "C",
+		src: "/images/stack/c-color.svg",
+		alt: "C",
+		url: "https://en.cppreference.com/w/c",
+	},
+	{
+		name: "C++",
+		src: "/images/stack/cplusplus-color.svg",
+		alt: "C++",
+		url: "https://en.cppreference.com/w/cpp",
+	},
+	{
+		name: "Java",
+		src: "/images/stack/java-color.svg",
+		alt: "Java",
+		url: "https://dev.java/",
+	},
+	{
+		name: "Kotlin",
+		src: "/images/stack/kotlin-color.svg",
+		alt: "Kotlin",
+		url: "https://kotlinlang.org/",
+	},
+]
